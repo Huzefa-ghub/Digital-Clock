ec6bfe16a20caf13f2611c8e60b89ef58a1fdd91
huzefa <huzefashyk2672@gmail.com>
2024-10-13 15:47:54 +0000

    updated code

--- while.sh
+++ while.sh
@@ -3,7 +3,7 @@
 filename=$1
 a=1
 echo "Reading file $filename using while loop"
-while read file; do
-	echo "Line - $a : $line"
+while read line ; do
+	echo "Line - $a  $line"
 	a=$((a+1))
 done < $filename
