e04729f766308886dd27dbe778a95913c4c0c49d
huzefa <huzefashyk2672@gmail.com>
2024-10-13 15:53:51 +0000

    added few codes in while.sh and a new file -by huzefa

--- while.sh
+++ while.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+
 filename=$1
+
+echo "reading file from cat cmd : Filename = $filename"
+echo "------------------------------------------------"
+cat $filename
+
+
 a=1
+echo "------------------------------------------------"
 echo "Reading file $filename using while loop"
 while read line ; do
 	echo "Line - $a  $line"
 	a=$((a+1))
 done < $filename
+echo "------------------------------------------------"
+
